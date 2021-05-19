910027b88c491dc24ec02ca451397f8526b34504
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-19 07:56:38 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/lucasborges/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.lucasborges.git;
+
+/**
+ * @author STI
+ * exemplo de integração eclipse e github
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
